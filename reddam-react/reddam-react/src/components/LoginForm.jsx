b2417536f5ed8c2a7fc797ffd60d864884169a23
nyiko mthombeni <nyikondzalo29@gmail.com>
2023-11-17 22:18:37 +0200

    adding funnctinality for login and signup

--- reddam-react/reddam-react/src/components/LoginForm.jsx
+++ reddam-react/reddam-react/src/components/LoginForm.jsx
@@ -29,3 +29,69 @@ export default function LoginForm(props) {
         )
     }
 }
+/*
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "./Input";
+import { BiSolidUser } from "react-icons/bi";
+import { HiLockClosed } from "react-icons/hi";
+import Button from "./Button";
+
+export default function LoginForm(props) {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const processLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      // Make a POST request to your authentication endpoint
+      const response = await fetch("https://reddam.agreeableplant-3f520c83.southafricanorth.azurecontainerapps.io/teacherlogin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+
+      if (response.ok) {
+        // Successful login, navigate to the dashboard page
+        navigate("/dashboard/home");
+      } else {
+        // Handle unsuccessful login (display error message, redirect, etc.)
+        console.error("Login failed");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
+  if (props.isLogin) {
+    return (
+      <form className={`fixed top-0 left-[550px] right-0 bottom-0 flex flex-col items-center justify-center`}>
+        <h1 className="text-5xl font-semibold mb-[100px]">Log In</h1>
+        <Input
+          placeholder="Email"
+          icon={<BiSolidUser />}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Password"
+          icon={<HiLockClosed />}
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className="mt-[50px]">
+          <Button type="submit" label="Log In" onClick={processLogin} />
+        </div>
+      </form>
+    );
+  }
+}*/
+
--- reddam-react/reddam-react/src/components/SignUpForm.jsx
+++ reddam-react/reddam-react/src/components/SignUpForm.jsx
@@ -3,18 +3,55 @@ import Input from './Input'
 import Button from "./Button"
 import {BiRename, BiSolidRename, BiSolidUser} from "react-icons/bi"
 import {HiLockClosed} from "react-icons/hi"
+import axios from 'axios'
+import React, {useState} from 'react'
 
 export default function SignUpForm(props) {
+
+    const [formData, setFormData] = useState ({
+        email: '',
+        password: '',
+        name: '',
+        surname: ''
+    })
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try{
+            const url = 'https://reddam.agreeableplant-3f520c83.southafricanorth.azurecontainerapps.io/teacherregister'
+
+            const response = await axios.post(url, formData, {
+                headers:{
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            console.log(response.data);
+
+        }catch(error){
+            console.error('Error during registration', error);
+        }
+    };
+
+    const handleChange = (e) => {
+        const{ name, value} =e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    }
+
     if (!props.isLogin) {
         return (
             <form className={`fixed top-0 right-[550px] left-0 bottom-0 flex flex-col items-center justify-center`}>
                 <h1 className="text-5xl font-semibold mb-[100px]">Sign Up</h1>
-                <Input placeholder="Name" icon={<BiRename />} />
-                <Input placeholder="Surname" icon={<BiSolidRename />} />
-                <Input placeholder="Email" icon={<BiSolidUser />} />
-                <Input placeholder="Password" icon={<HiLockClosed />} />
+                <Input placeholder="Name" icon={<BiRename />} onChange={handleChange} name="name" value={formData.name} />
+                <Input placeholder="Surname" icon={<BiSolidRename />} onChange={handleChange} name="surname" value={formData.surname}/>
+                <Input placeholder="Email" icon={<BiSolidUser />}  onChange={handleChange} name="email" value={formData.email} />
+                <Input placeholder="Password" icon={<HiLockClosed />} onChange={handleChange} name="password" value={formData.password} />
                 <div className="mt-[50px]">
-                    <Button type="" label="Sign Up" />
+                    <Button type="" label="Sign Up"/>
                 </div>
                 
             </form>
